bed74908fa2fb2f1c15a0401714e637a0d27e39d
Lenovo <adityapermono8@gmail.com>
2024-12-19 23:34:21 +0700

    Membuat fungsi FormMahasiswa untuk input data mahasiswa

---
+++ app/src/main/java/com/example/roomdatabase/ui/view/mahasiswa/InsertMhsView.kt
@@ -0,0 +1,138 @@
+package com.example.roomdatabase.ui.view.mahasiswa
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.roomdatabase.ui.viewmodel.FormErrorState
+import com.example.roomdatabase.ui.viewmodel.MahasiswaEvent
+
+
+@Preview(showBackground = true)
+@Composable
+fun FormMahasiswa(
+    mahasiswaEvent: MahasiswaEvent = MahasiswaEvent(),
+    onValueChange: (MahasiswaEvent) -> Unit = {},
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+) {
+    val jenisKelamin = listOf("Laki Laki", "Perempuan")
+    val kelas = listOf("A", "B", "C", "D", "E")
+
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mahasiswaEvent.nama,
+            onValueChange = { onValueChange(mahasiswaEvent.copy(nama = it)
+            ) },
+            label = { Text(text = "Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text(text = "Masukkan Nama") }
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mahasiswaEvent.nim,
+            onValueChange = { onValueChange(mahasiswaEvent.copy(nim = it)
+            ) },
+            label = { Text(text = "NIM") },
+            isError = errorState.nim != null,
+            placeholder = { Text(text = "Masukkan Nim") }
+        )
+        Text(
+            text = errorState.nim ?: "",
+            color = Color.Red
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Jenis Kelamin")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            jenisKelamin.forEach { jk ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = mahasiswaEvent.jeniskelamin == jk,
+                        onClick = {
+                            onValueChange(mahasiswaEvent.copy(jeniskelamin = jk)) }
+                    )
+                    Text(text = jk,
+                    )
+                }
+            }
+        }
+        Text(
+            text = errorState.jeniskelamin ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mahasiswaEvent.alamat,
+            onValueChange = { onValueChange(mahasiswaEvent.copy(alamat = it)
+            ) },
+            label = { Text(text = "Alamat") },
+            isError = errorState.alamat != null,
+            placeholder = { Text(text = "Masukkan Alamat") }
+        )
+        Text(
+            text = errorState.alamat ?: "",
+            color = Color.Red
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Kelas")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            kelas.forEach { kls ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = mahasiswaEvent.kelas == kls,
+                        onClick = {
+                            onValueChange(mahasiswaEvent.copy(kelas = kls)) }
+                    )
+                    Text(text = kls,
+                    )
+                }
+            }
+        }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mahasiswaEvent.angkatan,
+            onValueChange = { onValueChange(mahasiswaEvent.copy(angkatan = it)
+            ) },
+            label = { Text(text = "Angkatan") },
+            isError = errorState.angkatan != null,
+            placeholder = { Text(text = "Masukkan Angkatan") }
+        )
+        Text(
+            text = errorState.angkatan ?: "",
+            color = Color.Red
+        )
+    }
+}
