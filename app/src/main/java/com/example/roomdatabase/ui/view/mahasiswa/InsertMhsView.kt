2ba5b336eeaf9fb3b8bcdfaa87d5c339b7abd592
Lenovo <adityapermono8@gmail.com>
2024-12-19 23:41:13 +0700

    Membuat fungsi InsertMhsView

--- app/src/main/java/com/example/roomdatabase/ui/view/mahasiswa/InsertMhsView.kt
+++ app/src/main/java/com/example/roomdatabase/ui/view/mahasiswa/InsertMhsView.kt
@@ -4,22 +4,34 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomdatabase.ui.customwidget.TopAppBar
 import com.example.roomdatabase.ui.navigation.AlamatNavigasi
 import com.example.roomdatabase.ui.viewmodel.FormErrorState
 import com.example.roomdatabase.ui.viewmodel.MahasiswaEvent
+import com.example.roomdatabase.ui.viewmodel.MahasiswaViewModel
 import com.example.roomdatabase.ui.viewmodel.MhsUIState
+import kotlinx.coroutines.launch
 
 
 @Preview(showBackground = true)
@@ -169,4 +181,58 @@ fun InsertBodyMhs(
 
 object DestinasiInsert : AlamatNavigasi {
     override val route: String = "insert_mhs"
+}
+
+@Composable
+fun  InserMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState // Ambil UI State dari ViewModel
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    val coroutineScope = rememberCoroutineScope() // Untuk menjalankan coroutine
+
+    // Observasi perubahan snackbarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ){
+            padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Mahasiswa"
+            )
+            // Isi Body
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        viewModel.saveData()
+                    }
+                    onNavigate()
+                }
+            )
+        }
+    }
 }
