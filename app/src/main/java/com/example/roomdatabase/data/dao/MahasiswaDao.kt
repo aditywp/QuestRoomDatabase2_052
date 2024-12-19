27c708002a3b44bea575c13a86e3eed61656d6e1
Lenovo <adityapermono8@gmail.com>
2024-12-19 22:32:40 +0700

    menambahkan fungsi untuk mengambil semua data mahasiswa secara urut ASC

--- app/src/main/java/com/example/roomdatabase/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/data/dao/MahasiswaDao.kt
@@ -2,7 +2,9 @@ package com.example.roomdatabase.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.roomdatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
@@ -11,4 +13,6 @@ interface MahasiswaDao {
     suspend fun insertMahasiswa(
         mahasiswa: Mahasiswa
     )
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 }
