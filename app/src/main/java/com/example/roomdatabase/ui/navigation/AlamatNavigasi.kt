c7eb19d8823e53993a020065c3a5d021d390d301
Lenovo <adityapermono8@gmail.com>
2024-12-19 23:17:37 +0700

    Menambahkan AlamatNavigasi, destinasi (home, detail, update) dengan parameter NIM

---
+++ app/src/main/java/com/example/roomdatabase/ui/navigation/AlamatNavigasi.kt
@@ -0,0 +1,22 @@
+package com.example.roomdatabase.ui.navigation
+
+
+interface AlamatNavigasi {
+    val route : String
+}
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi{
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : AlamatNavigasi{
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
