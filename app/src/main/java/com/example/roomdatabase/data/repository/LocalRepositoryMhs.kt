0ecfea1b49b997794b5117883e0344a083d6b735
Lenovo <adityapermono8@gmail.com>
2024-12-19 22:54:52 +0700

    Menambahkan LocalRepositoryMhs untuk mengelola data mahasiswa dengan MahasiswaDao ada fungsi insert,getallMhs,getMhs,deleteMhs, dan updateMhs

---
+++ app/src/main/java/com/example/roomdatabase/data/repository/LocalRepositoryMhs.kt
@@ -0,0 +1,31 @@
+package com.example.roomdatabase.data.repository
+
+import com.example.roomdatabase.data.dao.MahasiswaDao
+import com.example.roomdatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+
+class LocalRepositoryMhs(
+    private val mahasiswaDao: MahasiswaDao
+) : RepositoryMhs {
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+
+}
